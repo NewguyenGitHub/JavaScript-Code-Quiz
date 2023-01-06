8dee18dca304e68becc0c552ea9b92c1eb35c93d
NewguyenGitHub <MNguyen8.mn@gmail.com>
2023-01-06 04:01:54 -0700

    add starter code

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+// Create array of questions
+let questions = [
+  {
+    numb: 1,
+    question: "What is not a commonly used data type?",
+    answer: "alerts",
+    options: [
+      "strings",
+      "booleans",
+      "alerts",
+      "numbers"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What is the condition in an if/else statement enclosed with?",
+    answer: "parenthesis",
+    options: [
+      "quotes",
+      "curly brackets",
+      "parenthesis",
+      "square brackets"
+    ]
+  },
+  {
+    numb: 3,
+    question: "In JavaScript, what can arrays store?",
+    answer: "all of the above",
+    options: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What do string values need to be enclosed with when being assigned to variables?",
+    answer: "quotes",
+    options: [
+      "commas",
+      "curly brackets",
+      "quotes",
+      "parenthesis"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What useful tool is used during development and debugging for printing content to the debugger?",
+    answer: "console.log",
+    options: [
+      "JavaScript",
+      "terminal/bash",
+      "for loops",
+      "console.log"
+    ]
+  }
+]
